2ddc9c6572bf29409b45a41ac3715274a9c0032b
Max Asnaashari <max.asnaashari@canonical.com>
2024-09-16 02:29:23 +0000

    service: Use predictable wordlist with test tag
    Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>

---
+++ service/test_wordlist.go
@@ -0,0 +1,11 @@
+//go:build test
+
+package service
+
+// Provided by the EFF under the Creative Commons Attribution License.
+// License: http://creativecommons.org/licenses/by/3.0/us/
+// Source: https://www.eff.org/files/2016/09/08/eff_short_wordlist_2_0.txt
+var wordlist = `1	a
+2	a
+3	a
+4	a`
